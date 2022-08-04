6b86f112acb5c5a8e089fd7ff25fff76a25710ac
Jiyu Hong <hhongjiyu@gmail.com>
2022-08-04 17:39:15 +0900

    FreeBoardListAction

---
+++ src/main/java/action/FreeBoardListAction.java
@@ -0,0 +1,37 @@
+package action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vo.ActionForward;
+
+public class FreeBoardListAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ActionForward forward = null;
+		
+		// 페이징 처리를 위한 변수선언
+		int pageNum = 1;
+		int listLimit = 10;
+		int pageLimit = 10;
+		
+		// pageNum 이 전달 됐을 경우 가져와서 변수에 저장
+		if(request.getParameter("pageNum") != null) {
+			pageNum = Integer.parseInt(request.getParameter("pageNum"));
+		}
+		
+		// 페이징 처리에 필요한 전체 게시물 갯수 조회 작업 요청
+		FreeBoardListService service = new FreeBoardListService();
+		int listCount = service.getListCount();
+		
+		
+		
+		
+		
+		
+		
+		return forward;
+	}
+
+}
