a448f1d3e4a445a7d2be93790c582525419ef49d
joojoo1226 <joojoo1226@naver.com>
2022-08-06 00:38:18 +0900

    CampaignReviewBlockDTO 수정, CommunityFrontController-캠페인후기목록, CampaignReviewListAction 틀만 생성

--- src/main/java/controller/CommunityFrontController.java
+++ src/main/java/controller/CommunityFrontController.java
@@ -7,8 +7,7 @@ import javax.servlet.annotation.*;
 import javax.servlet.http.*;
 
 import action.*;
-import action.community.FreeBoardListAction;
-import action.community.FreeBoardWriteProAction;
+import action.community.*;
 import vo.*;
 
 //커뮤니티(캠페인 후기, 반려나무 성장일기, 큐앤에이, 공지사항, 자유게시판) 컨트롤러
@@ -24,25 +23,32 @@ public class CommunityFrontController extends HttpServlet {
 		Action action = null; // Action 클래스 인스턴스를 관리하는 변수
 		ActionForward forward = null; // 포워딩 정보를 관리하는 변수
 		
-		if(command.equals("/FreeBoardWriteForm.cm")) {
+		if(command.equals("/FreeBoardWriteForm.cm")) { //자유게시판 작성 폼
 			forward = new ActionForward();
 			forward.setPath("freeboard/freeboard_write.jsp");
 			forward.setRedirect(false);
-		} else if (command.equals("/FreeBoardWritePro.cm")) {
+		} else if (command.equals("/FreeBoardWritePro.cm")) { //자유게시판 작성 동작
 			try {
 				action = new FreeBoardWriteProAction();
 				forward = action.execute(request, response);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		} else if(command.equals("/FreeBoardList.cm")) {
+		} else if(command.equals("/FreeBoardList.cm")) { //자유게시판 목록
 			try {
 				action = new FreeBoardListAction();
 				forward = action.execute(request, response);
 			} catch (Exception e) {
 				e.printStackTrace(); 
 			}
-		} 
+		} else if(command.equals("/CampaignReviewList.cm")) { //캠페인후기 목록
+			action = new CampaignReviewListAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		
 		if(forward != null) { //포워딩 처리
 			if(forward.isRedirect()) { //redirect 방식
--- src/main/java/vo/community/CampaignReviewBlockDTO.java
+++ src/main/java/vo/community/CampaignReviewBlockDTO.java
@@ -1,14 +1,36 @@
 package vo.community;
 
 /*
- * 아직 미완이니 가져가지 마시오.
+ * 캠페인리뷰 신고자, 신고하는 내용, 신고 이유
 CREATE TABLE cam_re_block(
-	cam_re_id (fk)
-	cam_re_content (fk)
-	cam_re_block_reason
+	cam_re_id VARCHAR(50) REFERENCES member(mem_id),
+	cam_re_content VARCHAR(100) REFERENCES campaign_review(cam_re_content),
+	cam_re_block_reason VARCHAR(200)
 ); 
 */
 
 public class CampaignReviewBlockDTO {
-
+	private String cam_re_id;
+	private String cam_re_content;
+	private String cam_re_block_reason;
+	
+	public String getCam_re_id() {
+		return cam_re_id;
+	}
+	public void setCam_re_id(String cam_re_id) {
+		this.cam_re_id = cam_re_id;
+	}
+	public String getCam_re_content() {
+		return cam_re_content;
+	}
+	public void setCam_re_content(String cam_re_content) {
+		this.cam_re_content = cam_re_content;
+	}
+	public String getCam_re_block_reason() {
+		return cam_re_block_reason;
+	}
+	public void setCam_re_block_reason(String cam_re_block_reason) {
+		this.cam_re_block_reason = cam_re_block_reason;
+	}
+	
 }
