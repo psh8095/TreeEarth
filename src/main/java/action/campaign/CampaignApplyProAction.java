66c4dc75273b7f6a90d33bb693b9aa336cabf3fc
user <user@DESKTOP-2PGRMF4>
2022-08-08 18:59:52 +0900

    3

---
+++ src/main/java/action/campaign/CampaignApplyProAction.java
@@ -0,0 +1,21 @@
+package action.campaign;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import action.Action;
+import vo.ActionForward;
+
+public class CampaignApplyProAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("CampaignApplyProAction");
+		ActionForward forward = null;
+		
+		
+		
+		return forward;
+	}
+
+}
---
+++ src/main/webapp/campaign/campaign_apply.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<!-- 헤더 -->
+	<jsp:include page="../hf/header.jsp"></jsp:include>
+	<!-- 헤더 -->
+	<h1>캠페인 참가 신청서 양식</h1>
+	<!-- 푸터 -->
+	<jsp:include page="../hf/footer.jsp"></jsp:include>
+	<!-- 푸터 -->
+</body>
+</html>
