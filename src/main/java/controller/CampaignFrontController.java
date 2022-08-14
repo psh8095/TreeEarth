e3dbefcbc5cc24fedd9268b4dcb36f4f8c140df4
user <user@DESKTOP-VEM53O3>
2022-08-14 16:14:28 +0900

    Create: svc.campaign 폴더 생성 후 작업 / CampaignWriteProAction파일 생성 후 작업 Update: CampaignFormController 수정 작업

--- src/main/java/controller/CampaignFrontController.java
+++ src/main/java/controller/CampaignFrontController.java
@@ -10,47 +10,96 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import action.Action;
-import action.campaign.CampaignApplyProAction;
-import action.campaign.CampaignExpiredListAction;
-import action.campaign.CampaignRecruListAction;
+import action.campaign.*;
 import vo.ActionForward;
 
-// 캠페인 컨트롤러
+//캠페인 컨트롤러(모집중인 캠페인 목록 조회, 종료된 캠페인 목록 조회, 캠페인 참가 신청서 작성, 캠페인 아이디어 공모글 작성, 캠페인 공고 작성/수정/삭제)
 @WebServlet("*.cp")
 public class CampaignFrontController extends HttpServlet {
-	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("CampaignFrontController");
-		request.setCharacterEncoding("UTF-8");
-		String command = request.getServletPath();
-		System.out.println("서블릿 주소 : " + command);
-		
-		Action action = null;
-		ActionForward forward = new ActionForward();
-		
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, 
+	IOException {
+			System.out.println("CampaignFrontController");
+			request.setCharacterEncoding("UTF-8");
+			String command = request.getServletPath();
+			System.out.println("서블릿 주소 : " + command);
+			
+			Action action = null; 
+			ActionForward forward = new ActionForward();
+			
+		//-------------------------------------------------------------------------------------
 		if(command.equals("/CampaignRecruList.cp")) {
-			// 모집중인 캠페인 서블릿 주소
-			action = new CampaignRecruListAction();
+				// 모집중인 캠페인 서블릿 주소
+				action = new CampaignRecruListAction();
 			try {
 				forward = action.execute(request, response);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		} else if(command.equals("/CampaignExpiredList.cp")) {
-			// 종료된 캠페인 서블릿 주소
-			action = new CampaignExpiredListAction();
+				// 종료된 캠페인 서블릿 주소
+				action = new CampaignExpiredListAction();
 			try {
 				forward = action.execute(request, response);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		} else if(command.equals("/CampaignApplyForm.cp")) {
-			// 캠페인 참가 신청서 서블릿 주소
-			forward = new ActionForward();
-			forward.setPath("campaign/campaign_apply.jsp");
+				// 캠페인 참가 신청서 서블릿 주소
+				forward = new ActionForward();
+				forward.setPath("campaign/campaign_apply.jsp");
 		} else if(command.equals("/CampaignApplyPro.cp")) {
-			// 캠페인 참가 신청서 서블릿 주소
-			action = new CampaignApplyProAction();
-
+				// 캠페인 참가 신청서 서블릿 주소
+				action = new CampaignApplyProAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if(command.equals("/CampaignWriteForm.cp")){//캠페인 작성 폼
+				forward = new ActionForward();
+				forward.setPath("campaign/campaign_write.jsp");
+				forward.setRedirect(false);
+		} else if(command.equals("/CampaignWritePro.cp")) {//캠페인 작성 동작
+				action = new CampaignWriteProAction();
+				try {
+					forward = action.execute(request, response);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+		} else if(command.equals("/CampaignList.cp")) {//캠페인 목록
+//				action = new CampaignListAction();
+				try {
+					forward = action.execute(request, response);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+		} else if(command.equals("/CampaignDetail.cp")) {//캠페인 상세조회
+//				action = new CampaignDetailAction();
+			try {
+				forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if(command.equals("/CampaignModifyForm.cp")) {//캠페인 수정 폼
+//				action = new CampaignModifyFormAction();
+				try {
+					forward = action.execute(request, response);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+		} else if(command.equals("/CampaignModifyPro.cp")) {//캠페인 수정 동작
+//				action = new CampaignModifyProAction();
+				try {
+					forward = action.execute(request, response);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+		} else if(command.equals("/CampaignDeleteForm.cp")) {//캠페인 삭제 폼
+				 forward = new ActionForward();
+				 forward.setPath("/campaign/campaign_delete.jsp");
+				 forward.setRedirect(false);
+		} else if(command.equals("/CampaignDeletePro.cp")) {//캠페인 삭제 동작
+//				action = new CampaignDeleteProAction();
 			try {
 				forward = action.execute(request, response);
 			} catch (Exception e) {
@@ -58,10 +107,13 @@ public class CampaignFrontController extends HttpServlet {
 			}
 		}
 		
+		
+		//==============================================================================
+		//포워딩 작업 수행하기 위한 공통코드
 		if(forward != null) {
-			if(forward.isRedirect()) {
+			if(forward.isRedirect()) {//redirect 방식
 				response.sendRedirect(forward.getPath());
-			} else {
+			} else {//dispatcher 방식
 				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
 				dispatcher.forward(request, response);
 			}
