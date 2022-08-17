c89860609091082e0a478b94add73c63e8a6fa6b
user <user@DESKTOP-2PGRMF4>
2022-08-17 16:58:45 +0900

    결제 기능 구현

---
+++ src/main/java/action/store/InsertOrderAction.java
@@ -0,0 +1,51 @@
+package action.store;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import action.Action;
+import svc.store.insertOrderService;
+import vo.ActionForward;
+import vo.store.OrderDTO;
+import vo.store.OrderDetailDTO;
+
+public class InsertOrderAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("InsertOrderAction");
+		ActionForward forward = null;
+		
+		HttpSession session = request.getSession();
+		String sId = session.getAttribute("sId").toString();
+//		System.out.println(request.getParameter("order_id"));
+//		System.out.println(request.getParameter("mem_name"));
+//		System.out.println(request.getParameter("mem_phone"));
+//		System.out.println(request.getParameter("mem_email"));
+//		System.out.println(Integer.parseInt(request.getParameter("amount")));
+//		System.out.println(request.getParameter("mem_address"));
+		
+		OrderDTO order = new OrderDTO();
+		order.setOrder_id(request.getParameter("order_id"));
+		order.setMem_id(sId);
+		order.setMem_name(request.getParameter("mem_name"));
+		order.setMem_phone(request.getParameter("mem_phone"));
+		order.setMem_email(request.getParameter("mem_email"));
+		order.setAmount(Integer.parseInt(request.getParameter("amount")));
+		order.setMem_address(request.getParameter("mem_address"));
+		
+		insertOrderService service = new insertOrderService();
+		service.insertOrder(order);
+		
+		OrderDetailDTO orderDetail = new OrderDetailDTO();
+		orderDetail.setOrder_id(request.getParameter("order_id"));
+		orderDetail.setSto_idx(Integer.parseInt(request.getParameter("sto_idx")));
+		orderDetail.setQuantity(Integer.parseInt(request.getParameter("quantity")));
+		
+		service.insertOrderDetail(orderDetail);
+		
+		return forward;
+	}
+
+}
